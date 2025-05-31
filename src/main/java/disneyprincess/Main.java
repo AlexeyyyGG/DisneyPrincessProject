cba8fad39b9a950d2e2cb0839d4c8334356fa6eb
AlexeyyyGG <leonovalexey555@gmail.com>
2025-05-31 12:45:03 +0300

    add CommandDispatcher

--- src/main/java/disneyprincess/Main.java
+++ src/main/java/disneyprincess/Main.java
@@ -1,20 +1,30 @@
 package disneyprincess;
 
+import disneyprincess.controller.Controller;
+import disneyprincess.utils.CommandDispatcher;
 import disneyprincess.utils.ConsolReader;
 import disneyprincess.model.Princess;
 import disneyprincess.repository.PrincessRepository;
 import disneyprincess.utils.PrincessFileReader;
 import java.util.List;
 
-public class Runner {
+public class Main {
   public static void main(String[] args) {
     PrincessRepository repository = new PrincessRepository();
-
+    ConsolReader reader = new ConsolReader();
+    Controller controller = new Controller();
     String filename = "disneyPrincesses";
     List<Princess> princessesList = PrincessFileReader.readPrincessesFromFile(filename);
     repository.addAll(princessesList);
 
-    ConsolReader.start(repository);
+    while (true) {
+      reader.read();
+      String command = reader.getCommand();
+      if (command.equalsIgnoreCase("exit")) {
+        System.out.println("Exiting...");
+        break;
+      }
+      CommandDispatcher.dispatch(command, reader.getArgs(), repository, controller);
+    }
   }
-
 }
--- src/main/java/disneyprincess/utils/ConsolReader.java
+++ src/main/java/disneyprincess/utils/ConsolReader.java
@@ -1,74 +1,30 @@
 package disneyprincess.utils;
 
-import disneyprincess.commands.AddCommand;
-import disneyprincess.controller.Controller;
-import disneyprincess.commands.DeleteCommand;
-import disneyprincess.commands.GetCommand;
-import disneyprincess.commands.ListCommand;
-import disneyprincess.commands.UpdateCommand;
-import disneyprincess.model.EyeColor;
-import disneyprincess.model.HairColor;
-import disneyprincess.model.Princess;
-import disneyprincess.repository.PrincessRepository;
 import java.util.Scanner;
 
 public class ConsolReader {
-  public static void start(PrincessRepository repository) {
-    Scanner scanner = new Scanner(System.in);
-    Controller controller = new Controller();
+  private String[] args;
+  private String command;
+  private final Scanner scanner;
 
-    while (true) {
-      System.out.println(
-          "Enter the command: add/update/delete/get/list/exit");
-      String input = scanner.nextLine();
-      String[] parts = input.split(" ");
-      String action = parts[0];
+  public ConsolReader(){
+    this.scanner = new Scanner(System.in);
+  }
 
-      switch (action.toLowerCase()) {
-        case "add":
-          int id = Integer.parseInt(parts[1]);
-          String name = parts[2];
-          int age = Integer.parseInt(parts[3]);
-          HairColor hairColor = HairColor.fromString(parts[4]);
-          EyeColor eyeColor = EyeColor.fromString(parts[5]);
-          AddCommand addCommand = new AddCommand(repository,
-              new Princess(id, name, age, hairColor, eyeColor));
-          controller.setCommand(addCommand);
-          controller.executeCommand();
-          break;
-        case "update":
-          int updateId = Integer.parseInt(parts[1]);
-          String updateName = parts[2];
-          int updateAge = Integer.parseInt(parts[3]);
-          HairColor updateHairColor = HairColor.fromString(parts[4]);
-          EyeColor updateEyeColor = EyeColor.fromString(parts[5]);
-          UpdateCommand updateCommand = new UpdateCommand(updateId, updateName, updateAge,
-              updateHairColor, updateEyeColor, repository);
-          controller.setCommand(updateCommand);
-          controller.executeCommand();
-          break;
-        case "delete":
-          int deleteId = Integer.parseInt(parts[1]);
-          DeleteCommand deleteCommand = new DeleteCommand(repository, deleteId);
-          controller.setCommand(deleteCommand);
-          controller.executeCommand();
-          break;
-        case "get":
-          int getID = Integer.parseInt(parts[1]);
-          GetCommand getCommand = new GetCommand(repository, getID);
-          controller.setCommand(getCommand);
-          controller.executeCommand();
-          break;
-        case "list":
-          ListCommand listCommand = new ListCommand(repository);
-          controller.setCommand(listCommand);
-          controller.executeCommand();
-          break;
-        case "exit":
-          return;
-        default:
-          System.out.println("Unknown command");
-      }
-    }
+  public  void read() {
+    System.out.println("Enter the command: add/update/delete/get/list/exit");
+    String input = scanner.nextLine();
+    String[] parts = input.split("\\s+");
+    this.command = parts[0].toLowerCase();
+    this.args = new String[parts.length - 1];
+    System.arraycopy(parts, 1, this.args, 0, parts.length - 1);
+  }
+
+  public String[] getArgs() {
+    return args;
+  }
+
+  public String getCommand() {
+    return command;
   }
 }
